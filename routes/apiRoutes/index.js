71b9fe84f8c0e6dfc545338eb716fe927f76c224
Mark Younan <markyounan@gmail.com>
2020-05-12 13:32:00 -0700

    fixed delete route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-const{ getWorkout, addWorkout, deleteWorkout } = require('./../../controllers/workoutController');
+const { getWorkout, addWorkout, deleteWorkout } = require('./../../controllers/workoutController');
 
 // '/api' prepended
 
 router.route('/workout')
-.get(getWorkout)
-.post(addWorkout)
-.delete(deleteWorkout)
+    .get(getWorkout)
+    .post(addWorkout)
+
+router.route('./workout/:workoutId')
+    .delete(deleteWorkout)
 
 module.exports = router;
