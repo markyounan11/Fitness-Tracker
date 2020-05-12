a82ba0ee2a1e57af7717892dc7aea4b06f5ca767
Mark Younan <markyounan@gmail.com>
2020-05-12 11:07:59 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+//morgan dev tool
+app.use(morgan("dev"));
+//set up middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
+​
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+    //useUnifiefdTopology: true
+});
+// '/' automatically prepended
+app.use( routes );
+​
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port: ${PORT}`);
+});
